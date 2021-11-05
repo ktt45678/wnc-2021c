b653f267e5c82dd33531f59c2400622b7ab94b66
ktt45678 <ktt45678@gmail.com>
2021-11-05 14:19:17 +0700

    Add redirects file for netlify and update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://morgan-api.herokuapp.com/',
+  apiUrl: 'https://morgan.demona.live',
   recaptchaSiteKey: '6Ld7TL8cAAAAAKjLTRRnOiM8nuTiEHX5pxwongXP'
 };
